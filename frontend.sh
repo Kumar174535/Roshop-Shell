8ebfb7041ca9d7d82776a992752dedaebf223876
kumar174535 <kumarsanisetty@duck.com>
2024-11-12 13:30:02 +0530

    frontend script

---
+++ frontend.sh
@@ -0,0 +1,21 @@
+#list modules
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+#start & enable service
+systemctl enable nginx
+systemctl start nginx
+
+#remove the default content
+rm -rf /usr/share/nginx/html/*
+
+#download the frontend content
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+#extract the frontend content
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+#restart service
+systemctl restart nginx
