5f57d2db70e0b3b1ee40a1598ff308155914c0ac
kumar174535 <kumarsanisetty@duck.com>
2025-01-03 14:36:02 +0530

    updated

--- common.sh
+++ common.sh
@@ -15,29 +15,29 @@ app_prerequisites(){
   echo $?
 
   print_heading "downloading content"
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>log_file
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
   cd /app
   echo $?
 
   print_heading "extract content"
-  unzip /tmp/$app_name.zip &>>log_file
+  unzip /tmp/$app_name.zip &>>$log_file
   cd /app
   echo $?
 
 }
 
 print_heading() {
-  echo -e "$color $1 $no_color"  &>>/tmp/roboshop.log
+  echo -e "$color $1 $no_color"  &>>$log_file
   echo -e "$color $1 $no_color"
 }
 
 service_start(){
-  systemctl daemon-reload &>>log_file
-  systemctl enable $app_name &>>log_file
-  systemctl restart $app_name &>>log_file
+  systemctl daemon-reload &>>$log_file
+  systemctl enable $app_name &>>$log_file
+  systemctl restart $app_name &>>$log_file
 }
 
 service_no_daemon(){
-  systemctl enable $app_name &>>log_file
-  systemctl restart $app_name &>>log_file
+  systemctl enable $app_name &>>$log_file
+  systemctl restart $app_name &>>$log_file
 }
--- payment.sh
+++ payment.sh
@@ -3,18 +3,18 @@ app_name=payment
 
 
 print_heading "Copy Payment Service"
-cp payment.service /etc/systemd/system/payment.service &>>log_file
+cp payment.service /etc/systemd/system/payment.service &>>$log_file
 echo $?
 
 print_heading "Install python3"
-dnf install python3 gcc python3-devel -y &>>log_file
+dnf install python3 gcc python3-devel -y &>>$log_file
 echo $?
 
 #function_name
 app_prerequisites
 
 print_heading "installing dependencies"
-pip3 install -r requirements.txt &>>log_file
+pip3 install -r requirements.txt &>>$log_file
 echo $?
 
 print_heading "restarting services"
